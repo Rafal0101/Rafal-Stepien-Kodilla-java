69f00905c323fadb98dccad678f4f43057494ad0
Rafal0101 <rst@op.pl>
2017-09-05 21:04:40 +0200

    Added calculator test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,27 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Modu 6. Wprowadzenie do testowania oprogramowania.");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUserName();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK!");
+        } else {
+            System.out.println("Error.");
+        }
+
+        Calculator calculator = new Calculator();
+        int addResult = calculator.sum(4,11);
+        int subtractResult = calculator.subtract(100, 1);
+        if (addResult == 15 && subtractResult == 99) {
+            System.out.println("Calculator test OK.");
+        } else {
+            System.out.println("Calculator test error.");
+        }
     }
 }

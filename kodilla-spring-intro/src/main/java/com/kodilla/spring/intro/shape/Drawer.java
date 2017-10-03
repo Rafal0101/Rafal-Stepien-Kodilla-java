cc9b7293614fcadcaa31cb7fef172af39dc0d55d
Rafal0101 <rst@op.pl>
2017-10-03 20:42:22 +0200

    Exercise 10.1

---
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Drawer.java
@@ -0,0 +1,25 @@
+package com.kodilla.spring.intro.shape;
+
+public class Drawer {
+    final Shape shape;
+
+    public Drawer(Shape shape) {
+        this.shape = shape;
+    }
+
+    public String doDrawing() {
+        return shape.draw();
+    }
+
+    /*
+    public void doDrawing() {
+        Shape shape;
+
+        shape = new Circle();
+        shape.draw();
+
+        shape = new Triangle();
+        shape.draw();
+    }
+    */
+}
---
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -0,0 +1,34 @@
+package com.kodilla.spring.forum;
+/*
+Zadanie: Bean ForumUser
+
+Celem zadania jest stworzenie w Kontenerze Beana klasy ForumUser i jego sprawdzenie w teście. Aby zrealizować zadanie:
+
+W katalogu src/main/java utwórz pakiet com.kodilla.spring.forum
+Napisz klasę ForumUser w pakiecie com.kodilla.spring.forum. Klasa powinna mieć pole String username inicjowane
+w konstruktorze wartością "John Smith" (nie przekazuj tego jako parametr - wpisz to w konstruktorze "na sztywno").
+Oznacz klasę jako @Component.
+W katalogu src/test/java stwórz pakiet com.kodilla.spring.forum, a w nim zestaw testów ForumUserTestSuite.
+Napisz test testGetUsername() pobierający z kontekstu aplikacji uchwyt do Beana klasy ForumUser. Test powinien
+pobierać username i sprawdzać przy pomocy asercji czy zwróconą wartością jest "John Smith".
+*/
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ForumUserTestSuite {
+    @Test
+    public void testGerUsername() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.forum");
+        ForumUser forumUser = context.getBean(ForumUser.class);
+
+        //When
+        String result = forumUser.getUserName();
+
+        //Then
+        Assert.assertEquals(result, "John Smith");
+    }
+}

b92e5064d9d52d0ad1f4993869b0775786dcd666
Rafal0101 <rst@op.pl>
2017-10-14 22:55:43 +0200

    Exercise 11.2 lifting

--- kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -9,41 +9,46 @@ Zmodyfikuj test testGetBooks() w taki sposób, aby prezentował działanie shall
  */
 
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.time.LocalDate;
 
 public class LibraryTestSuite {
-    @Test
-    public void testGetBooks() {
-        //Given
-        Book book1 = new Book("Sherlock Holmes1", "A. C. Doyle", LocalDate.of(1901, 10, 5));
-        Book book2 = new Book("Sherlock Holmes2", "A. C. Doyl", LocalDate.of(1910, 11, 25));
-        Book book3 = new Book("Sherlock Holmes3", "A. C. Doy", LocalDate.of(1902, 12, 15));
-        Book book4 = new Book("Sherlock Holmes4", "A. C. Do", LocalDate.of(1909, 1, 6));
-        Book book5 = new Book("Sherlock Holmes5", "A. C. D", LocalDate.of(1903, 3, 30));
+    private static Library library = null;
+    private static Book book1 = null;
+    private static Book book2 = null;
+    private static Book book3 = null;
+    private static Book book4 = null;
+    private static Book book5 = null;
 
-        Library library = new Library("Library Number One.");
+
+    @Before
+    public void createTestData() {
+        book1 = new Book("Sherlock Holmes1", "A. C. Doyle", LocalDate.of(1901, 10, 5));
+        book2 = new Book("Sherlock Holmes2", "A. C. Doyl", LocalDate.of(1910, 11, 25));
+        book3 = new Book("Sherlock Holmes3", "A. C. Doy", LocalDate.of(1902, 12, 15));
+        book4 = new Book("Sherlock Holmes4", "A. C. Do", LocalDate.of(1909, 1, 6));
+        book5 = new Book("Sherlock Holmes5", "A. C. D", LocalDate.of(1903, 3, 30));
+
+        library = new Library("Library Number One.");
         library.getBooks().add(book1);
         library.getBooks().add(book2);
         library.getBooks().add(book3);
         library.getBooks().add(book4);
         library.getBooks().add(book5);
+    }
+
+    @Test
+    public void testShallowClone() {
+        //Given
 
         //shallow cloning
         Library shallowClonedLibrary = null;
         try {
             shallowClonedLibrary = library.shallowCopy();
             shallowClonedLibrary.setName("Library number Two.");
-        } catch (CloneNotSupportedException e) {
-            System.out.println(e);
-        }
-
-        //deep cloning
-        Library deepClonedLibrary= null;
-        try {
-            deepClonedLibrary = library.deepCopy();
-            deepClonedLibrary.setName("Library number Three.");
         } catch (CloneNotSupportedException e) {
             System.out.println(e);
         }
@@ -56,9 +61,29 @@ public class LibraryTestSuite {
         //Then
         System.out.println(library);
         System.out.println(shallowClonedLibrary);
-        System.out.println(deepClonedLibrary);
+
         Assert.assertEquals(2, library.getBooks().size());
         Assert.assertEquals(2, shallowClonedLibrary.getBooks().size());
+    }
+
+    @Test
+    public void testDeepClone() {
+        //Given
+
+        //When
+        //deep cloning
+        Library deepClonedLibrary= null;
+        try {
+            deepClonedLibrary = library.deepCopy();
+            deepClonedLibrary.setName("Library number Three.");
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+
+        //Then
+        System.out.println(library);
+        System.out.println(deepClonedLibrary);
+        Assert.assertEquals(5, library.getBooks().size());
         Assert.assertEquals(5, deepClonedLibrary.getBooks().size());
     }
 }
